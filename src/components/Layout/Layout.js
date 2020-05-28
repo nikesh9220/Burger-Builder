c47ef9fac478eca355451c42b9e30fb8f70d8512
Nikesh Pandya <Nikesh.Pandya@civica2.com>
2020-05-28 15:24:50 +0530

    Logo added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Aux from '../../hoc/Auxilory'
 import classes from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 const layout = (props) =>(
  <Aux>
+    <Toolbar/>
     <div >Toolbar ,Sidedrawer, Backdrop</div>
+    
         <main className={classes.Content}>
          {props.children}
         </main> 
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import burgerLogo from '../../assets/images/Logo.png'
+
+const logo = () =>(
+    <div>
+        <img src={burgerLogo}/>
+    </div>
+);
+
+export default logo;
